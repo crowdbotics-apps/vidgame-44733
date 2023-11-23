d308333f2911e4292fb732e91d6c9fac927d05c0
Crowdbotics <team@crowdbotics.com>
2023-11-23 11:35:43 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "vidgame-44733",
+  url: "https://vidgame-44733.botics.co",
+  api: "https://vidgame-44733.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
